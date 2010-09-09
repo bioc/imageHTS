6eec275add29669a23736e8ace997fa412fc21f7
Gregoire Pau <gregoire.pau@gmail.com>
2010-09-09 18:33:33 +0000

    minor fix for 0.99.3
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/imageHTS@49316 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/extractFeatures.R
+++ R/extractFeatures.R
@@ -9,7 +9,7 @@ extractFeatures = function(x, uname, featurePar, access='cache') {
     seg  = try(readHTS(x, 'seg', uname=u, access=access))  
     if (class(cal)!='try-error' & class(seg)!='try-error') {
       ftrs = eval(call(efMethod, cal, seg))
-      fftrs = fileHTS(x, 'ftrs', uname=u, createPath=TRUE)
+      fftrs = fileHTS(x, 'ftrs', uname=u, createPath=TRUE, access='local')
       write.table(ftrs, file=fftrs, sep='\t', quote=FALSE, row.names=FALSE, col.names=TRUE)
       cat(' OK\n')
     } else cat('NA\n')
