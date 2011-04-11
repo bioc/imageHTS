875e383d2bf2eb9ce9fd877ac6c6f768412e3db8
Gregoire Pau <gregoire.pau@gmail.com>
2011-04-11 18:51:12 +0000

    added makeCellHTS
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/imageHTS@54696 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/makeCellHTS.R
@@ -0,0 +1,13 @@
+makeCellHTS = function(x, profiles, measurementNames, name) {
+  if (!is.data.frame(profiles)) stop("'profiles' must a be a data frame containing the phenotypic profiles")
+  if (ncol(profiles)>10) stop('too many features (columns) in phenotypic profiles')
+  if (!'uname'%in%colnames(profiles)) stop("'profiles' must contain the 'uname' field")
+  prw = uname2prw(profiles$uname)
+  well = rowcol2well(prw$row, prw$col)
+  profiles$uname = NULL
+  xd = data.frame(plate=prw$plate, replicate=prw$replicate, well=well, profiles)
+  if (missing(measurementNames)) y = buildCellHTS2(xd)
+  else y = buildCellHTS2(xd, measurementNames)
+  if (missing(name)) name(y) = name
+  y
+}
